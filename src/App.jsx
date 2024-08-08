021e47ca1a37598f4117e09544a74396b96b0b87
Joy Addisi <elooghenejoy@gmail.com>
2024-08-09 00:33:12 +0100

    setup: react-routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import React from "react";
-
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
 const App = () => {
-  return <h1 className="underline text-green-500 text-2xl">Hello World pls work</h1>;
+  return (
+    <main className="bg-slate-300/20">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={"Home"} />
+          <Route path="/about" element={"About"} />
+          <Route path="/projects" element={"Projects"} />
+          <Route path="/contact" element={"Contact"} />
+          <Route path="*" element={"NotFound"} />
+        </Routes>
+      </Router>
+    </main>
+  );
 };
 
 export default App;
