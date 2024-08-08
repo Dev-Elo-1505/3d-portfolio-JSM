30f00c2db42240b598cc4a86334da87c7524414f
Joy Addisi <elooghenejoy@gmail.com>
2024-08-09 00:50:11 +0100

    Build Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <header className="header">
+      <NavLink
+        to="/"
+        className="w-10 h-10 bg-white rounded-lg flex items-center justify-center font-bold shadow-md"
+      >
+        <p className="blue-gradient_text">AJ</p>
+      </NavLink>
+      <nav className="flex text-lg gap-7 font-medium">
+        <NavLink to="/about" className={({ isActive }) => isActive ? 'text-blue-500' : 'text-black'}>
+        About
+        </NavLink>
+        <NavLink to="/projects" className={({ isActive }) => isActive ? 'text-blue-500' : 'text-black'}>
+        Projects
+        </NavLink>
+      </nav>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
