5dfdd27bde272fa2af7d1e3eb5bb27d36baf7594
Joy Addisi <elooghenejoy@gmail.com>
2024-08-09 01:11:25 +0100

    fix: import statement

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
+          <Route path="/contact" element={<Contacts />} />
           <Route path="*" element={"NotFound"} />
         </Routes>
       </Router>
