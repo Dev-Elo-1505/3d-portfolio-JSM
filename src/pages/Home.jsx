ff49720e5956ca8a45ed949a046eabd05b6daac1
Elo-oghene <elooghenejoy@gmail.com>
2024-08-10 09:00:31 +0100

    adjust island for screen sizes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,17 +9,40 @@ import Island from "../models/Island";
 </div> */
 }
 const Home = () => {
+  const adjustIslandForScreenSize = () => {
+    let screenScale = null;
+    let screenPosition = [0, -6.5, -43];
+    let rotation = [0.1, 4.7, 0]
+
+    if (window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
+    } else {
+      screenScale = [1, 1, 1];
+    }
+
+    return [screenScale, screenPosition, rotation];
+  };
+
+  const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize();
+
   return (
     <section className="w-full h-screen relative">
-      <Canvas className="w-full h-screen bg-transparent" camera={{ near: 0.1, far: 1000 }}>
+      <Canvas
+        className="w-full h-screen bg-transparent"
+        camera={{ near: 0.1, far: 1000 }}
+      >
         <Suspense fallback={<Loader />}>
-        <directionalLight />
-        <ambientLight />
-        <pointLight />
-        <spotLight />
-        <hemisphereLight />
+          <directionalLight />
+          <ambientLight />
+          <pointLight />
+          <spotLight />
+          <hemisphereLight />
 
-        <Island />
+          <Island
+          position= {islandPosition}
+          scale = {islandScale}
+          rotation = {islandRotation}
+           />
         </Suspense>
       </Canvas>
     </section>
