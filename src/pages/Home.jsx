15d673413a64b0e0a6db48de065cb97615998f70
Joy Addisi <elooghenejoy@gmail.com>
2024-08-10 07:47:06 +0100

    setup 3d island model

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import Loader from "../components/Loader";
+import Island from "../models/Island";
 
 {
   /* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
@@ -11,7 +12,15 @@ const Home = () => {
   return (
     <section className="w-full h-screen relative">
       <Canvas className="w-full h-screen bg-transparent" camera={{ near: 0.1, far: 1000 }}>
-        <Suspense fallback={<Loader />}></Suspense>
+        <Suspense fallback={<Loader />}>
+        <directionalLight />
+        <ambientLight />
+        <pointLight />
+        <spotLight />
+        <hemisphereLight />
+
+        <Island />
+        </Suspense>
       </Canvas>
     </section>
   );
