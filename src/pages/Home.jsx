b4565ff9a44a75e447061a360e9cbecb35d7ca90
Elo-oghene <elooghenejoy@gmail.com>
2024-08-11 22:54:11 +0100

    implement plane rotation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,6 +13,7 @@ import Bird from "../models/Bird";
 }
 const Home = () => {
   const [isRotating, setIsRotating] = useState(false);
+  const [currentStage, setCurrentStage] = useState(1);
 
   const adjustIslandForScreenSize = () => {
     let screenScale = null;
@@ -68,6 +69,7 @@ const Home = () => {
             rotation={islandRotation}
             isRotating={isRotating}
             setIsRotating = {setIsRotating}
+            setCurrentStage={setCurrentStage}
           />
           <Plane
           isRotating={isRotating}
