8cb76770516e67d617d36eab538033b963399be9
Elo-oghene <elooghenejoy@gmail.com>
2024-08-15 21:44:54 +0100

    Implement bird rotation

--- src/models/Bird.jsx
+++ src/models/Bird.jsx
@@ -1,14 +1,21 @@
-import { useGLTF } from '@react-three/drei';
-import birdScene from '../assets/3d/bird.glb';
+import { useRef, useEffect } from "react";
+import { useAnimations, useGLTF } from "@react-three/drei";
+import birdScene from "../assets/3d/bird.glb";
 
 const Bird = () => {
-const { scene, animations } = useGLTF(birdScene);
+  const { scene, animations } = useGLTF(birdScene);
+  const birdRef = useRef();
+  const { actions } = useAnimations(animations, birdRef);
 
-    return (
-    <mesh position={[-5, 2, 1]} scale={[0.003, 0.003, 0.003]}>
-        <primitive object={scene} />
+  useEffect(() => {
+    actions["Take 001"].play();
+  }, []);
+
+  return (
+    <mesh position={[-5, 2, 1]} scale={[0.003, 0.003, 0.003]} ref={birdRef}>
+      <primitive object={scene} />
     </mesh>
-)
-}
+  );
+};
 
-export default Bird
+export default Bird;
