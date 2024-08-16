ea4cbd6f236678f9dec484e1f33cc63f63167832
Elo-oghene <elooghenejoy@gmail.com>
2024-08-16 04:02:47 +0100

    Build projects page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,14 +8,24 @@ const Navbar = () => {
         to="/"
         className="w-10 h-10 bg-white rounded-lg flex items-center justify-center font-bold shadow-md"
       >
-        <p className="blue-gradient_text">AJ</p>
+        <p className="blue-gradient_text ">AJ</p>
       </NavLink>
       <nav className="flex text-lg gap-7 font-medium">
-        <NavLink to="/about" className={({ isActive }) => isActive ? 'text-blue-500' : 'text-black'}>
-        About
+        <NavLink
+          to="/about"
+          className={({ isActive }) =>
+            isActive ? "text-blue-500" : "text-black"
+          }
+        >
+          About
         </NavLink>
-        <NavLink to="/projects" className={({ isActive }) => isActive ? 'text-blue-500' : 'text-black'}>
-        Projects
+        <NavLink
+          to="/projects"
+          className={({ isActive }) =>
+            isActive ? "text-blue-500" : "text-black"
+          }
+        >
+          Projects
         </NavLink>
       </nav>
     </header>
