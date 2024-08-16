fe618343f698332320921cebcff963a37b2c2591
Elo-oghene <elooghenejoy@gmail.com>
2024-08-16 02:11:39 +0100

    Implement skills effect

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+
+import { skills } from "../constants";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <section className="max-container">
+      <h1 className="head-text">
+        Hello, I'm{" "}
+        <span className="blue-gradient_text font-semibold drop-shadow">
+          Elo-oghene
+        </span>
+      </h1>
+      <div className="mt-5 flex flex-col gap-3 text-slate-500">
+        <p>
+          Software Engineer based in Lagos, Nigeria specializing in Full-Stack
+          development. I build quality softwares that people need.
+        </p>
+      </div>
+      <div className="py-10 flex flex-col">
+        <h3 className="subhead-text">My Skills</h3>
+        <div className="mt-16 flex flex-wrap gap-12">
+          {skills.map((skill) => (
+            <div className="block-container w-20 h-20">
+              <div className="btn-back rounded-xl" />
+              <div className="btn-front rounded-xl flex justify-center items-center">
+                <img
+                  src={skill.imageUrl}
+                  alt={skill.name}
+                  className="w-1/2 h-1/2 object-contain"
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default About
+export default About;
